8568a26ed1ea28a2eced17ad574f38a45f869f71
ChuongLeDuc <89777170+ChuongLeDuc@users.noreply.github.com>
2021-08-30 17:08:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php 
-   ob_start();
-   include "config.php";
-   if (!$_SESSION['login']){
-      header('location:login.php');
-      exit();
-}else{
-   $sql="UPDATE trangthai SET status='Trang 1' WHERE id='1'";
-     mysqli_query($conn,$sql);
-    }
-?>
+
   <html>
    <head>
        <link rel="stylesheet" type="text/css" href="style.css" />
